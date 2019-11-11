377403cfd9065e9a7d958d2813f732d024e006ad
Chris N <chris@tnwebdev.com>
2019-11-11 03:45:28 -0800

    Fixed display time lable on HTML page

--- javascript/trainscheduler.js
+++ javascript/trainscheduler.js
@@ -49,7 +49,7 @@ $('#btn-submit').on('click', function (event) {
 });
 
 // Create Firebase event for adding train schedule to database and table in HTML
-database.ref().on('child_added'), function (childSnapshot) {
+database.ref().on('child_added', function (childSnapshot) {
   console.log(childSnapshot.val());
 
   // Store data to a variable.
@@ -64,14 +64,45 @@ database.ref().on('child_added'), function (childSnapshot) {
   console.log(trainTime);
   console.log(trainFreq);
 
-  // Something the moment time setup
+  // Time Schedule 
+  // Assumptions
+  var tFrequency = 3;
+
+  // Time is 3:30 AM
+  var firstTime = "03:30";
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
+
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
   // Create the new row for new train schedule data
   var newRow = $('<tr>').append(
     $('<td>').text(trainName),
     $('<td>').text(trainDest),
     $('<td>').text(trainTime),
-    $('<td>').text(trainFreq)
+    $('<td>').text(trainFreq),
+    $('<td>').text(nextTrain),
+    $('<td>').text(tMinutesTillTrain)
   );
 
   // Appand the new row to the table
